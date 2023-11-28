197886a1cea243f2036204f5af72235e5239d3cf
Administrator <dr.sobhan.esmae>
2023-11-28 17:27:49 +0330

    Step 01

---
+++ Program_05.py
@@ -0,0 +1,33 @@
+
+# my_lanbda_01 = lambda x: x*2
+# print(my_lanbda_01(2))
+
+# my_list = [2, 4, 6, 8]
+# my_lanbda_02 = lambda number: number * 2
+# new_list = list(map(my_lanbda_02, my_list))
+# print(new_list)
+
+
+# num = 0.25
+# print(id(num))
+
+# text = "Sobhan Esmaeili"
+# print(type(text))
+
+# class person():
+#     def __init__(self, first_name, last_name):
+#         self.name = first_name
+#         self.family = last_name
+#         self.age = 35
+#     def __str__(self):
+#         return "My Name is {} and my Family is {}".format(self.name, self.family)
+#
+# class employee(person):
+#     pass
+#
+# my_emploee = employee("Sobhan", "Esmaeili")
+# print(my_emploee)
+
+
+
+
